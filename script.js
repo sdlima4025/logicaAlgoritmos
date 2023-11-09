8cab4ab8125ed164f38fc7a22e25348e34dd8f7e
sdlima4025 <sergiolimafg3@gmail.com>
2023-11-09 10:55:16 -0300

    exercicio 01/02/03

--- script.js
+++ script.js
@@ -17,10 +17,37 @@
 
 //exercicio 03
 
-function somaMedia() {
-    const idade1 = parseInt( document.querySelector('#idade-1').value);
-    const idade2 = parseInt(document.querySelector('#idade-2').value);
+// function somaMedia() {
+//     const idade1 = parseInt( document.querySelector('#idade-1').value);
+//     const idade2 = parseInt(document.querySelector('#idade-2').value);
     
-    let media = (idade1 + idade2)/2;
-    console.log(media);
+//     let media = (idade1 + idade2)/2;
+//     console.log(media);
+// }
+
+//exercicio 04
+
+function calcular() {
+    const a = parseInt(document.querySelector('#value1').value);
+    const b = parseInt(document.querySelector('#value2').value);
+    
+    const selectOperation = document.querySelector('input[name="operation"]:checked').value;
+    var nameOperation, calculo;
+    if(selectOperation == 'adicao') {
+        nameOperation = 'Adição';
+        calculo = "A Adição dos números é  = "  + (a + b);
+    }
+    if(selectOperation == 'subtracao') {
+        nameOperation = 'subtração';
+        calculo = "A Subtração dos números é  = " + (a - b);
+    }
+    if(selectOperation == 'multiplicacao') {
+        nameOperation = 'Multiplicação';
+        calculo = "A Multiplicação dos números é  = " + (a * b);
+    }
+    if(selectOperation == 'divisao') {
+        nameOperation = 'Divisão';
+        calculo = "A divisão dos números é  = " + (a / b);
+    }
+    alert(calculo);
 }
